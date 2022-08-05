3ee26b090818e6ff2b8154537332050602bd24c3
LevKoganov <levk@playsmart.co.il>
2022-08-05 13:30:21 +0300

    function return boolean

--- functions/manageRole.js
+++ functions/manageRole.js
@@ -1,20 +1,26 @@
 const champSchema = require("../models/champion_schema");
 
-async function manageRole(interaction, championId, inputMember) {
+async function manageRole(interaction, championId, inputMember, championRoleName) {
   try {
-    const championRoleName = "KOTH Champion"; // Role id
-
     // Get Champion data
     const updatedPreviousChampData = await champSchema.find({ userId: championId });
     const role = interaction.guild.roles.cache.find((role) => role.name === championRoleName); // Role info
 
-    // Remove "role" if champion had no other titles
-    if (updatedPreviousChampData.length < 1) {
-      const member = await interaction.guild.members.fetch(championId);
-      member.roles.remove(role);
+    if (role) {
+      // Remove "role" if champion had no other titles
+      if (updatedPreviousChampData.length < 1) {
+        const member = await interaction.guild.members.fetch(championId);
+        await member.roles.remove(role);
+      }
+
+      // Add role to a user
+      await inputMember.roles.add(role);
+      return true
+
+    } else {
+      return false
     }
 
-    inputMember.roles.add(role); // Add role to a user
   } catch (err) {
     console.log(`manageRole err`, err);
   }
