094658c53b2c7740093e4f9723651256cb0cb216
LevKoganov <levk@playsmart.co.il>
2022-08-05 13:28:54 +0300

    checking if champion role exist

--- commands/setChampion.js
+++ commands/setChampion.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
 // Functions and imports
-const saveChampion = require("../functions/saveChampion")
-const manageRole = require("../functions/manageRole")
-const updateChampionsChannel = require("../functions/updateChampionsChannel")
-const championsChannelData = require("../functions/championsChannelData")
+const saveChampion = require("../functions/saveChampion");
+const manageRole = require("../functions/manageRole");
+const updateChampionsChannel = require("../functions/updateChampionsChannel");
+const championsChannelData = require("../functions/championsChannelData");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -56,86 +56,115 @@ module.exports = {
   ),
     
 	async execute(interaction) {
+    try {
+      // Server and Channel info
+      const guiildInfo = interaction.guild; // Guild info
+      const championChannelInfo = {};
+      let championChannelData = championChannelInfo[guiildInfo.id]; // Guild id as key
+  
+      // Input info
+      const game = interaction.options.getString("game"); // Game name
+      const selectedUser = interaction.options.getMember('champion');
+  
+      // Roles
+      const championRoleName = "KOTH - Champion";
+      const moderatorsRoleName = "Moderators";
+      let hasChampionRole = false;
+      let roleOutPutResult = ``;
+      const hasModRole = interaction.member.roles.cache.some((role) => role.name === moderatorsRoleName);
+       
+      // Check if user is mod or admin
+      if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator) || hasModRole) {
+  
+        // Return array of prev champion
+        const previousChampData = await saveChampion(selectedUser.user.username, selectedUser.user.id, game);
+  
+        // Check if there was a champion
+        if (previousChampData.length > 0) {
+          
+          // Role assignment
+          hasChampionRole = await manageRole(interaction, previousChampData[0].userId, selectedUser, championRoleName)
+          roleOutPutResult = `${hasChampionRole ? "" : 'champion DID NOT get role! make sure "KOTH - Champion" rolename exist.'}`
 
-    // Server and Channel info
-    const guiildInfo = interaction.guild; // Guild info
-    const championChannelInfo = {};
-    let championChannelData = championChannelInfo[guiildInfo.id]; // Guild id as key
-
-    // Input info
-    const game = interaction.options.getString("game"); // Game name
-    const selectedUser = interaction.options.getMember('champion')
-
-    // Role IDs
-    const championRoleName = "KOTH Champion"
-    const moderatorsRoleName = "Moderators"
-    const hasModRole = interaction.member.roles.cache.some((role) => role.name === moderatorsRoleName);
-     
-    // Check if user is mod or admin
-    if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator) || hasModRole) {
-
-      // Return array of prev champion
-      const previousChampData = await saveChampion(selectedUser.user.username, selectedUser.user.id, game);
-
-      // Check if there was a champion
-      if (previousChampData.length > 0) {
-        await manageRole(interaction, previousChampData[0].userId, selectedUser)
-
-        const championChannel = await championsChannelData(interaction, guiildInfo.id);
-
-        // Check if channel set.
-        if (!championChannel) {
-          return await interaction.reply({content: "champion is set.\nplease set a channel with '/set-channel-champion'.", ephemeral: true});
-
+          const championChannel = await championsChannelData(interaction, guiildInfo.id);
+          
+          // Check if channel set.
+          if (!championChannel) {
+            return await interaction.reply({
+              content: `champion is set.\nchampion channel IS NOT set!\n${roleOutPutResult}`,
+              ephemeral: true,
+            });
+  
+          } else {
+            // set data for channel
+            championChannelData = championChannelInfo[guiildInfo.id] = championChannel;
+  
+            // Check if bot have permissions
+            if (interaction.guild.members.me.permissionsIn(championChannel).has(PermissionsBitField.Flags.ViewChannel && PermissionsBitField.Flags.SendMessages)) {
+              await updateChampionsChannel(championChannelData);
+              
+            } else {
+              return await interaction.reply({
+                content: `champion is set.\nbot doesn't have a permission for "${championChannel.name}" channel\n${roleOutPutResult}`,
+                ephemeral: true,
+              });
+            }
+          }
+  
+          // Return successful reply
+          return await interaction.reply({content: `champion is set.\n${roleOutPutResult}`, ephemeral: true,});
+  
+        // If there was no champion
         } else {
-          // set data for channel
-          championChannelData = championChannelInfo[guiildInfo.id] = championChannel;
-
-          // Check if bot have permissions
-          if (interaction.guild.members.me.permissionsIn(championChannel).has(PermissionsBitField.Flags.ViewChannel && PermissionsBitField.Flags.SendMessages)) {
-            await updateChampionsChannel(championChannelData);
+          const role = interaction.guild.roles.cache.find(role => role.name === championRoleName); // Get the role ID
+          if (role) {
+            hasChampionRole = true;
+            await selectedUser.roles.add(role); // Add role to a user
+          } else {
+            hasChampionRole = false;
+          }
+  
+          roleOutPutResult = `${hasChampionRole ? "" : 'champion DID NOT get role! make sure "KOTH - Champion" rolename exist.'}`
+          const championChannel = await championsChannelData(interaction, guiildInfo.id);
+  
+          // Check if channel set.
+          if (!championChannel) {
+            return await interaction.reply({
+              content: `champion is set.\nchampion channel IS NOT set!\n${roleOutPutResult}`,
+              ephemeral: true,
+            });
 
           } else {
-            return await interaction.reply({content: `champion is set.\nbot doesn't have a permission for "${championChannel.name}" channel`, ephemeral: true});
+            // set data for channel
+            championChannelData = championChannelInfo[guiildInfo.id] = championChannel;
+  
+            // Check if bot have permissions
+            if (interaction.guild.members.me.permissionsIn(championChannel).has(PermissionsBitField.Flags.ViewChannel && PermissionsBitField.Flags.SendMessages)) {
+              await updateChampionsChannel(championChannelData);
+  
+            } else {
+              return await interaction.reply({
+                content: `champion is set.\nbot doesn't have a permission for "${championChannel.name}" channel\n${roleOutPutResult}`,
+                ephemeral: true,
+              });
+            }
           }
+  
+          // Return successful reply
+          return await interaction.reply({content: `champion is set.\n${roleOutPutResult}`, ephemeral: true,});
         }
-
-        // Return successful reply
-        return await interaction.reply({content: "champion is set.", ephemeral: true,});
-
-      // If there was no champion
+  
+        // User dosen`t have permissions for this command 
       } else {
-        const role = interaction.guild.roles.cache.find(role => role.name === championRoleName); // Get the role ID
-        selectedUser.roles.add(role); // Add role to a user
-
-        const championChannel = await championsChannelData(interaction, guiildInfo.id);
-
-        // Check if channel set.
-        if (!championChannel) {
-          return await interaction.reply({content: "champion is set.\nplease set a channel with '/set-channel-champion'.", ephemeral: true});
-        } else {
-          // set data for channel
-          championChannelData = championChannelInfo[guiildInfo.id] = championChannel;
-
-          // Check if bot have permissions
-          if (interaction.guild.members.me.permissionsIn(championChannel).has(PermissionsBitField.Flags.ViewChannel && PermissionsBitField.Flags.SendMessages)) {
-            await updateChampionsChannel(championChannelData);
-
-          } else {
-            return await interaction.reply({content: `champion is set.\nbot doesn't have a permission for "${championChannel.name}" channel`, ephemeral: true});
-          }
-        }
-
-        // Return successful reply
-        return await interaction.reply({content: "champion is set.", ephemeral: true,});
+          return interaction.reply({
+            content: `Only mods can use this command.`,
+            ephemeral: true,
+          });
       }
-
-    // User dosen`t have permissions for this command 
-    } else {
-        return interaction.reply({
-        content: "Only mods can use this command.",
-        ephemeral: true,
-      });
+      
+    } catch (err) {
+      console.log("setChampion:", err)
     }
+
 	},
 };
