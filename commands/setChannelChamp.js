29423e9d6620268167c94af2c2e55ffc8939d2e0
LevKoganov <levk@playsmart.co.il>
2022-08-05 13:29:42 +0300

    adding try & catch + await

--- commands/setChannelChamp.js
+++ commands/setChannelChamp.js
@@ -15,46 +15,51 @@ module.exports = {
     ),
 
   async execute(interaction) {
-    const moderatorsRoleName = "Moderators"
-    const hasModRole = interaction.member.roles.cache.some((role) => role.name === moderatorsRoleName); // Check if user is mod or admin
-
-    // Check if user is admin or mod
-    if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator) || hasModRole) {
-      // Channel info
-      const channel = interaction.options.getChannel("channel");
-
-      // Check for text channel
-      if (!channel || channel.type !== 0) {
+    try {
+      const moderatorsRoleName = "Moderators"
+      const hasModRole = interaction.member.roles.cache.some((role) => role.name === moderatorsRoleName); // Check if user is mod or admin
+  
+      // Check if user is admin or mod
+      if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator) || hasModRole) {
+        // Channel info
+        const channel = interaction.options.getChannel("channel");
+  
+        // Check for text channel
+        if (!channel || channel.type !== 0) {
+          return await interaction.reply({
+            content: "Please tag a text channel.",
+            ephemeral: true,
+          });
+        }
+  
+        // Add channel to DB
+        const setChannel = await championChannelSchema.findOneAndUpdate(
+          { _id: interaction.guild.id },
+          {
+            _id: interaction.guild.id,
+            channelId: channel.id,
+            channelName: channel.name,
+          },
+          { new: true, upsert: true, useFindAndModify: false }
+        );
+  
+        // Reply if channel is set
+        if (setChannel) {
+          return interaction.reply({
+            content: "Champion channel is set.",
+            ephemeral: true,
+          });
+        }
+  
+      } else {
         return interaction.reply({
-          content: "Please tag a text channel.",
+          content: "You dont have permission for this command...",
           ephemeral: true,
         });
       }
-
-      // Add channel to DB
-      const setChannel = await championChannelSchema.findOneAndUpdate(
-        { _id: interaction.guild.id },
-        {
-          _id: interaction.guild.id,
-          channelId: channel.id,
-          channelName: channel.name,
-        },
-        { new: true, upsert: true, useFindAndModify: false }
-      );
-
-      // Reply if channel is set
-      if (setChannel) {
-        return interaction.reply({
-          content: "Champion channel is set.",
-          ephemeral: true,
-        });
-      }
-
-    } else {
-      return interaction.reply({
-        content: "You dont have permission for this command...",
-        ephemeral: true,
-      });
+      
+    } catch (err) {
+      console.log("setChannelChampion:", err);
     }
 
   },
